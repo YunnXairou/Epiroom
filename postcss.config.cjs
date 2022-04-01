72f60ecccd6f95be7e713b736807b5540acd6434
Clément Beaubestre <clement+hub@beaubestre.fr>
2022-04-01 13:08:01 +0200

    Fix postcss nexting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
 	plugins: {
+		'postcss-import': {},
 		'tailwindcss/nesting': {},
 		tailwindcss: {},
+		'postcss-hexrgba': {},
 		'postcss-preset-env': {
 			features: { 'nesting-rules': false }
 		},
