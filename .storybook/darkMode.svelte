810303da17f4ee41bfe4591ce222b25f64d6dae0
Clément Beaubestre <clement+hub@beaubestre.fr>
2022-04-01 10:48:18 +0200

    :wrench: Add darkmode w/ nightwind

---
+++ .storybook/darkMode.svelte
@@ -0,0 +1,10 @@
+<script>
+	import addons from '@storybook/addons';
+
+	addons.getChannel().on('DARK_MODE', (v) => {
+		if (v) document.querySelector('#root').classList.add('dark');
+		else document.querySelector('#root').classList.remove('dark');
+	});
+</script>
+
+<slot />
