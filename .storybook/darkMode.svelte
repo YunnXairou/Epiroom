d341697244d234dc6da9b0774e9f124ee17764c7
Clément Beaubestre <clement+hub@beaubestre.fr>
2022-04-04 15:35:41 +0200

    :memo: Update example stories w/ darkmode

--- .storybook/darkMode.svelte
+++ .storybook/darkMode.svelte
@@ -1,6 +1,8 @@
 <script>
 	import addons from '@storybook/addons';
 
+	document.querySelector('#root').classList.add('nightwind');
+
 	addons.getChannel().on('DARK_MODE', (v) => {
 		if (v) document.querySelector('#root').classList.add('dark');
 		else document.querySelector('#root').classList.remove('dark');
