a7a18f53ce78e2d8162241206fa1e5d04dc927ef
Clément Beaubestre <clement+hub@beaubestre.fr>
2022-04-12 15:35:10 +0200

    :heavy_plus_sign: Add storybook-addon-paddings

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-svelte-csf',
+		'storybook-addon-paddings',
 		'storybook-fixtures',
 		'storybook-dark-mode',
 		{
