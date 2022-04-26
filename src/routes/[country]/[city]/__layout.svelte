f21e53e2564b8162481de939633b20c60751beb5
Clément Beaubestre <clement+hub@beaubestre.fr>
2022-04-26 11:21:20 +0200

    🩺 Add error messages for invalid slugs

---
+++ src/routes/[country]/[city]/__layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	export async function load({ params: { country, city }, stuff: { locations } }) {
+		if (!city.match('[a-zA-Z]{3}'))
+			return {
+				status: 404,
+				error: new Error(`The city code ${city} is not correctly formated`)
+			};
+
+		if (!(`${country}/${city}` in locations))
+			return {
+				status: 404,
+				error: new Error(`No school associated with the city code ${city}`)
+			};
+
+		return { status: 200 };
+	}
+</script>
+
+<slot />
--- src/routes/[country]/__layout.svelte
+++ src/routes/[country]/__layout.svelte
@@ -1,19 +1,24 @@
 <script lang="ts" context="module">
-	export async function load({ params: { country, city }, fetch }) {
-		if (!country.match('[a-zA-Z]{2}')) return { status: 404 };
-		if (!city.match('[a-zA-Z]{3}')) return { status: 404 };
+	export async function load({ params: { country }, fetch }) {
+		if (!country.match('[a-zA-Z]{2}'))
+			return {
+				status: 404,
+				error: new Error(`The country code ${country} is not correctly formated`)
+			};
 
 		const response = await fetch('/locations.json');
 		if (!response.ok) return { status: response.status };
 
 		const { locations } = await response.json();
-		if (!(`${country}/${city}` in locations)) return { status: 404 };
+		if (!(`${country}` in locations))
+			return {
+				status: 404,
+				error: new Error(`No school associated with the country code ${country}`)
+			};
 
 		return {
 			status: response.status,
-			stuff: {
-				locations: locations
-			}
+			stuff: { locations }
 		};
 	}
 </script>
